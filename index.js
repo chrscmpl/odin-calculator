d8292d673f4bf86308d6edb4090414f1451f6e41
chrscmpl <chcampolongo1@gmail.com>
2022-08-04 23:19:54 +0200

    Generalize pars function and add parse for powers

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
-function Expression(expr) {
-	expr = expr.replace(/\s/g, '');
-	this.expr = expr;
-	this.Terms = parseTerms(expr);
+/******************************
+ * Expression:                *
+ *      Term                  *
+ *      Expression "+" Term   *
+ *      Expression "-" term   *
+ * Term:                      *
+ *      Power                 *
+ *      Term "*" Power        *
+ *      Term "/" Power        *
+ *      Term "%" Power  	    *
+ * Power:                     *
+ *      Primary               *
+ *      Power "^" Primary     *
+ * 			Power "V" Primary     *
+ * Primary:                   *
+ *      Number                *
+ *      "(" Expression ")"    *
+ *      "[" Expression "]"    *
+ *      "{" Expression "}"    *
+ *      "|" Expression "|"    *
+ *      "-" Primary           *
+ *      "+" Primary           *
+ * Number:                    *
+ *      Floating-point-literal*
+ *****************************/
+
+function Expression(expression) {
+	expression = expression.replace(/\s/g, '');
+	this.str = expression;
+	this.terms = parse(expression, '+', '-');
+	this.parsedTerms = this.terms.map(term => new Term(term));
 }
 
-function parseTerms(expr) {
+function Term(term) {
+	this.term = term;
+	this.powers = parse(term, '^', 'V');
+	// this.parsedPowers = this.powers.map(power => new Power(power));
+}
+
+// function Power(power) {
+// 	this.power = power;
+// 	this.primaries = parse()
+// }
+
+function parse(str, ...separators) {
 	let res = [];
-	for (let i = 0; i < expr.length; i++) {
-		// for every + or - in the expression
-		if (expr[i] === '+' || expr[i] === '-') {
-			// if the + or - is not contained between parenthesis
-			if (!isWrapped(expr, i)) {
-				//append the term before + or - to the result and remove it from the expression
-				res.push(expr.slice(0, i));
-				expr = expr.slice(i + 1);
-				i = 0;
-			}
+	for (let i = 0; i < str.length; i++) {
+		// for every separator in the expression that is not
+		//wrapped between parenthesis
+		if (separators.some(sep => sep === str[i]) && !isWrapped(str, i)) {
+			//append the term before the separator to
+			//the result and remove it from the expression
+			res.push(str.slice(0, i));
+			str = str.slice(i + 1);
+			i = 0;
 		}
 	}
-	res.push(expr);
+	res.push(str);
 	return res;
 }
 
 // returns true if the index is wrapped between a pair of two matching parenthesis
-function isWrapped(expr, index) {
+function isWrapped(str, index) {
 	for (let i = 0; i < index; i++)
 		if (
-			(expr[i] === '(' ||
-				expr[i] === '[' ||
-				expr[i] === '{' ||
-				expr[i] === '|') &&
-			findMatchingBracket(expr, i) > index
+			(str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === '|') &&
+			findMatchingBracket(str, i) > index
 		)
 			return true;
 	return false;
@@ -39,11 +73,11 @@ function isWrapped(expr, index) {
 // if a matching bracket is found after the bracket and no
 //similar opening bracket was between them returns the index
 //of the matching bracket
-function findMatchingBracket(expr, bracket) {
-	const matching = matchingBracket(expr[bracket]);
-	for (let i = bracket + 1, similar = 0; i < expr.length; i++) {
-		if (expr[i] === expr[bracket]) similar++;
-		else if (expr[i] === matching) {
+function findMatchingBracket(str, bracket) {
+	const matching = matchingBracket(str[bracket]);
+	for (let i = bracket + 1, similar = 0; i < str.length; i++) {
+		if (str[i] === str[bracket]) similar++;
+		else if (str[i] === matching) {
 			if (similar) similar--;
 			else return i;
 		}
@@ -63,5 +97,6 @@ function matchingBracket(bracket) {
 	}
 }
 
-const expression = new Expression('1 + 2 * 3 + (4 + 2 + (2-3-6))');
+const expression = new Expression('1 ^ 6 + 2 * 3 ^ 5 + (4 + 2 + (2-3-6))');
 console.log(expression);
+expression.parsedTerms.forEach(term => console.log(term));
